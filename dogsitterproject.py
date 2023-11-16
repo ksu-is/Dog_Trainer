79ed593f520d2402e85b5cb012a42ee55548e714
jbords123 <142612656+jbords123@users.noreply.github.com>
2023-11-16 13:33:25 -0500

    Add files via upload

---
+++ dogsitterproject.py
@@ -0,0 +1,25 @@
+def good_behavior():
+    phase = "Good Job"
+    print(phase+"!")
+good_behavior()
+
+def bad_behavior():
+    phase = "No"
+    print(phase+"!")
+bad_behavior()
+
+def yell_this(phrase):
+    print(phrase upper() + "!")
+yell_this("Good Job")
+
+def scratch_door():
+    phase = "Dog needs to go outside"
+scratch_door():
+    
+def whine():
+    
+dog_name = input("Max: ").capitalize()
+if dog_name (barks):
+    print(No)
+elif dog_name (sits):
+    print(Good Job)
